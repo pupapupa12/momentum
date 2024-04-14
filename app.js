81010fcc57d239891e07740d6823721d47680df1
pupapupa12 <152703819+pupapupa12@users.noreply.github.com>
2024-04-14 15:02:55 +0900

    2.4_booleans

--- app.js
+++ app.js
@@ -1,13 +1,4 @@
-const a = 10;
-const b = 2;
+const amIFat = null;
+let something;
 
-let myName = "nico";
-
-console.log(a+b);
-console.log(a/b);
-console.log("hello " + myName);
-
-myName = "nicolas"
-// 'let'안 써도 상단과 같은 효과로 인식됨
-
-console.log("your name is " + myName);
+console.log(something, amIFat); 
