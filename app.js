c745bfc6a9470df81b76e8ae231f0b7b3514bca3
pupapupa12 <152703819+pupapupa12@users.noreply.github.com>
2024-04-14 14:47:59 +0900

    2.3_const and let

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const a = 10;
 const b = 2;
 
-const myName = "nico";
+let myName = "nico";
 
 console.log(a+b);
 console.log(a/b);
-console.log("hello" + myName);
+console.log("hello " + myName);
 
+myName = "nicolas"
+// 'let'안 써도 상단과 같은 효과로 인식됨
+
+console.log("your name is " + myName);
