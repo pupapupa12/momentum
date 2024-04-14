82337571bc6ed80e5958409a9180de853c16e0a1
pupapupa12 <152703819+pupapupa12@users.noreply.github.com>
2024-04-15 01:25:40 +0900

    2.5_arrays

--- app.js
+++ app.js
@@ -1,4 +1,23 @@
-const amIFat = null;
-let something;
+// const mon = "mon";
+// const tue = "tue";
+// const wed = "wed";
+// const thu = "thu";
+// const fri = "fri";
+// const sat = "sat";
+// const sun = "sun";
 
-console.log(something, amIFat); 
+const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat"];
+
+// get item from Array
+console.log(daysOfWeek);
+
+// add one more item from Array
+daysOfWeek.push("sun");
+
+console.log(daysOfWeek);
+
+
+const toBuy = ["potato", "tomato", "pizza"];
+toBuy.push("kimbab");
+
+console.log(toBuy[2]);
