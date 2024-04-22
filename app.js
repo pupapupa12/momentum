b8741486ca2c44eabcbbc4bad5c6d4e9616fbab2
pupapupa12 <152703819+pupapupa12@users.noreply.github.com>
2024-04-23 02:28:58 +0900

    2.11_Returns
    ㅋㅎㅋㅎㅋ.ㅋㅎ..ㅋ......

--- app.js
+++ app.js
@@ -1,19 +1,29 @@
 const calculator = {
   add: function (a, b) {
-    console.log(a + b);
+    return a + b;
   },
   minus: function (a, b) {
-    console.log(a - b);
+    return a - b;
+  },
+  times: function (a, b) {
+    return a * b;
   },
   divide: function (a, b) {
-    console.log(a / b);
+    return a / b;
   },
   square: function (a, b) {
-    console.log(a ** b);
+    return a ** b;
   },
 };
 
-calculator.add(3, 5);
-calculator.minus(19, 5);
-calculator.divide(8, 2);
-calculator.square(5, 2);
+const addResult = calculator.add(2, 3);
+const minusResult = calculator.minus(addResult, 10);
+const timesResult = calculator.times(10, minusResult);
+const divideResult = calculator.divide(timesResult, addResult);
+const squareResult = calculator.square(divideResult, minusResult);
+
+console.log(addResult);
+console.log(minusResult);
+console.log(timesResult);
+console.log(divideResult);
+console.log(squareResult);
