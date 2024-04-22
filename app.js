29242a2bfed7b510f57ce1b358462ac224b68341
pupapupa12 <152703819+pupapupa12@users.noreply.github.com>
2024-04-23 01:54:46 +0900

    2.10_Recap2
    계산기 만들기 숙제

--- app.js
+++ app.js
@@ -1,35 +1,19 @@
-// function에서 데이터 보내고 가져오는 방법
-function sayHello(nameOfPerson, age){
-    console.log("Hello may name is " + nameOfPerson + " and I'm " + age);
-}
-
-sayHello("nico", 10);
-sayHello("dal", 23);
-sayHello("lynn", 21);
-
-function plus(a, b){
-    console.log(a+b);
-}
-
-function divide(a, b){
+const calculator = {
+  add: function (a, b) {
+    console.log(a + b);
+  },
+  minus: function (a, b) {
+    console.log(a - b);
+  },
+  divide: function (a, b) {
     console.log(a / b);
-}
-
-plus(8, 60);
-divide(98, 20);
-
-
-const player = {
-    name: "nico",
-    sayHello: function(otherPersonName){
-        console.log("hello " + otherPersonName + " nice to meet you");
-    },
+  },
+  square: function (a, b) {
+    console.log(a ** b);
+  },
 };
 
-// object
-console.log(player.name);
-
-// function
-player.sayHello("lynn");
-player.sayHello("nico");
-
+calculator.add(3, 5);
+calculator.minus(19, 5);
+calculator.divide(8, 2);
+calculator.square(5, 2);
