e4e397cf4a1a0e5a229cfa2baf1b43768aae28cd
pupapupa12 <152703819+pupapupa12@users.noreply.github.com>
2024-04-23 15:12:22 +0900

    2.13_Conditionals

--- app.js
+++ app.js
@@ -1,29 +1,7 @@
-const calculator = {
-  add: function (a, b) {
-    return a + b;
-  },
-  minus: function (a, b) {
-    return a - b;
-  },
-  times: function (a, b) {
-    return a * b;
-  },
-  divide: function (a, b) {
-    return a / b;
-  },
-  square: function (a, b) {
-    return a ** b;
-  },
-};
+const age = parseInt(prompt("How old are you?"));
+// javascript는 윗문장에서 멈 춰서 기다리고 있음
+console.log(age);
 
-const addResult = calculator.add(2, 3);
-const minusResult = calculator.minus(addResult, 10);
-const timesResult = calculator.times(10, minusResult);
-const divideResult = calculator.divide(timesResult, addResult);
-const squareResult = calculator.square(divideResult, minusResult);
-
-console.log(addResult);
-console.log(minusResult);
-console.log(timesResult);
-console.log(divideResult);
-console.log(squareResult);
+// value의 type 확인 방법 => typeof
+// 다른 type으로 변환하는 방법 => parseInt
+// console.log(age, parseInt(age));
