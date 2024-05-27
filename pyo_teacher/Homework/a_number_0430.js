be27b0e490804fbcb01d77e89598bd5d53294d34
pupapupa12 <152703819+pupapupa12@users.noreply.github.com>
2024-05-28 03:03:30 +0900

    객체/정돈!

---
+++ pyo_teacher/Homework/a_number_0430.js
@@ -0,0 +1,5 @@
+// number
+
+// 시간 당 요금이 주어지면 일 요금 계산하기
+
+function dayRate(ratePerHour) {}
---
+++ pyo_teacher/Homework/p_number_0430.js
@@ -0,0 +1,22 @@
+// 이 코드를 복사하고 위 과제를 구현해보세요.
+
+/**
+ * 시간당 요금이 주어지면 일 요금 계산하기
+ *
+ * @param {number} ratePerHour
+ * @returns {number} 일 요금
+ */
+export function dayRate(ratePerHour) {
+    throw new Error("이 라인을 지우고 함수를 구현하세요");
+}
+
+/**
+ * 예산에 따라 일할 수 있는 날짜 수 구하기
+ *
+ * @param {number} budget: 전체 예산
+ * @param {number} ratePerHour: 시간당 요금
+ * @returns {number} 일할 수 있는 날짜 수
+ */
+export function daysInBudget(budget, ratePerHour) {
+    throw new Error("이 라인을 지우고 함수를 구현하세요");
+}
